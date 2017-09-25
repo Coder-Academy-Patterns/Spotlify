061251ac127739a5c5bf272bdb6479aaeb6aa755
Patrick Smith <patrick@burntcaramel.com>
2017-09-25 15:02:36 +1000

    Add validation to artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ApplicationRecord
+  # 'name' is required
+  # 'bio' has a max length of 8000 characters
+
+  validates :name, presence: true
+
+  #validates_presence_of :bio
+  #validates_length_of :bio, maximum: 8000
+  # or:
+  validates :bio, presence: true, length: { maximum: 8000 }
 end
