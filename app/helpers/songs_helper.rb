74abd8b7a09671184be992361810e1a204262207
Patrick Smith <patrick@burntcaramel.com>
2017-09-25 14:19:58 +1000

    Add formatting of song duration

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,10 @@
 module SongsHelper
+  # Format e.g.
+  # 89 as 1:29
+  # 127 as 2:07
+  def format_duration(seconds)
+    mm = seconds / 60
+    ss = '%02d' % (seconds % 60)
+    "#{mm}:#{ss}"
+  end
 end
